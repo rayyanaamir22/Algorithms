ea3173dc0ab151777e0e2ab6b6dd24c1998252e7
rayyanaamir22 <92763211+rayyanaamir22@users.noreply.github.com>
2022-11-10 15:14:29 -0500

    Add files via upload

--- Sorting Algorithms/mergeSort.py
+++ Sorting Algorithms/mergeSort.py
@@ -1,34 +1,40 @@
 # Merge sort
 
-def mergeSort(array):
-    if len(array) > 1:
-        middle = len(array)//2 # Find midpoint 
+def mergeSort(array: list) -> None:
+    '''
+    Given a list, sort it using the merge sort algorithm
+    '''
+    if (len(array) > 1): # Base case
+        middle = len(array)//2 # Midpoint 
 
-        left = array[:middle] # Store each half of the array
+        # Store each half of the array
+        left = array[:middle] 
         right = array[middle:]
 
-        mergeSort(left) # Merge sort each half recursively
+        # Merge sort each half recursively
+        mergeSort(left) 
         mergeSort(right)
 
         i = j = k = 0 # Counters to ensure all elements are merged
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]: # If left is smaller
-                array[k] = left[i] # Append 
+        while ((i < len(left)) and (j < len(right))):
+            if (left[i] < right[j]): # If left is smaller
+                array[k] = left[i] # Reassing in array
                 i += 1
             else: # If right is smaller 
-                array[k] = right[j] # Append
+                array[k] = right[j] # Reassign in array
                 j += 1
-
             k += 1 
 
         # Check any remaining elements
-        while i < len(left):
+        while (i < len(left)):
             array[k] = left[i]
             i += 1
             k += 1
-        while j < len(right):
+        while (j < len(right)):
             array[k] = right[j]
             j += 1
             k += 1
 
-    return array
+arr = [4,3,67,21,23,4,0,1]
+mergeSort(arr)
+print(arr)
